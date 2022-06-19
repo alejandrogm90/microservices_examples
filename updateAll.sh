02f1dde523e8dcd7673a473a139e863854dd7334
alejandrogm90 <alejandrogomezmartin90@gmail.com>
2022-06-19 14:12:25 +0200

    * Update updateAll.sh

--- updateAll.sh
+++ updateAll.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-git submodule sync
-git submodule update --init --recursive
+DIR_HOME=$(cd `dirname $0` && pwd)
+
+if [ -f "${DIR_HOME}/ssh_examples/scripts/commonFunctions.sh" ] ; then
+    git submodule sync
+    git submodule update --init --recursive
+    git pull --recurse-submodules
+else 
+    git submodule sync
+    git submodule update --init --recursive
+fi
